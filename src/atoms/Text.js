1abbeda55a8306fe905132526c7f98fc6b0d38f7
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-09-14 19:34:06 -0700

    adds props to Text atom

--- src/atoms/Text.js
+++ src/atoms/Text.js
@@ -3,15 +3,28 @@ import Box from './Box';
 
 export default
 class Text extends React.Component {
-  static propTypes = {
+  static propTypes = Object.assign({}, Box.propTypes, {
     children: PropTypes.any,
+    size: PropTypes.string,
+    color: PropTypes.string,
+    weight: PropTypes.string,
+  })
+
+  static defaultProps = {
+    size: 'inherit',
+    color: 'inherit',
+    weight: 'inherit',
   };
 
   render(){
+    var {
+      size, style, color, weight,
+    ...props} = this.props;
     return (
       <Box
         inline
-        {...this.props}
+        {...props}
+        style={Object.assign({weight, color, fontSize: size}, style)}
         childrenKey={this.props.children}
       />
     );
